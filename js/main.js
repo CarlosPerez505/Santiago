3fd86b5aab3c72237055fad9b4a6fd7ebaa59ca1
Carlos <carlosperez505@gmail.com>
2023-10-27 10:53:50 -0600

    Working on showcase and header

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var i = 0;
+var txt = 'Santiago';
+var speed = 50;
+
+function typeWriter() {
+    if (i < txt.length) {
+        document.getElementById("sant").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed);
+    }
+}
+
+
+
